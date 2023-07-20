9b5b46753f10889db0f31b3e3d17bdff353d1e3c
Sushil Behera <sbehera@mpgbis.com>
2023-07-20 13:35:29 +0530

    update css

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,8 @@
 import { useEffect, useState } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
-import '../styles/globals.css';
+// import '../styles/globals.css';
+import "../public/css/style.css";
 import Layout from '../components/layout/Layout';
 import "swiper/css";
 import "swiper/css/navigation";
